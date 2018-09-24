28d5b6a9ce463df0ae139c548a51d7f4d038dff7
ecjtusbs <ecjtusbs@foxmail.com>
2018-09-24 10:27:36 +0800

    基本矩形覆盖问题

--- 矩形覆盖.java
+++ 矩形覆盖.java
@@ -6,3 +6,18 @@
  * @Last modified time: 2018-09-22T21:28:44+08:00
  * @Copyright: ecjtusbs@foxmail.com
  */
+ public class Solution {
+    public int RectCover(int target) {
+
+        if (target < 0) {
+            return  -1;
+        }
+
+        if (target <= 2) {
+            return target;
+        }
+
+        //第一块竖放时总的放法+第一块横放时总的放法
+        return RectCover(target - 1) + RectCover(target - 2);
+    }
+}
