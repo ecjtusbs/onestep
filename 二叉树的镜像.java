ec29af0dbb38082b0d9450f7397702d702791729
ecjtusbs <ecjtusbs@foxmail.com>
2018-09-25 18:48:51 +0800

    二叉树的镜像，递归算法

---
+++ 二叉树的镜像.java
@@ -0,0 +1,25 @@
+/**
+ * @Author: chenhaisheng
+ * @Date:   2018-09-25T18:45:56+08:00
+ * @Email:  ecjtusbs@foxmail.com
+ * @Last modified by:   chenhaisheng
+ * @Last modified time: 2018-09-25T18:47:32+08:00
+ * @Copyright: ecjtusbs@foxmail.com
+ */
+
+题目描述
+操作给定的二叉树，将其变换为源二叉树的镜像。
+输入描述:
+二叉树的镜像定义：
+    源二叉树
+     	    8
+     	   /  \
+     	  6   10
+     	 / \  / \
+     	5  7 9 11
+    镜像二叉树
+     	    8
+     	   /  \
+     	  10   6
+     	 / \  / \
+     	11 9 7  5
